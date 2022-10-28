6df6580794478e2a537fe2e9a410372fcc7549da
Ajarek <ajarek@poczta.onet.pl>
2022-10-29 01:23:41 +0200

    type added

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,116 @@
 import { createContext, useContext, useState } from 'react'
-import  Form from './components/Form/Form'
+import Form from './components/Form/Form'
 import Header from './components/Header/Header'
+
+
 export type GlobalContent = {
   newRecipe: boolean
-  setNewRecipe:(c: boolean) => void
+  setNewRecipe: (c: boolean) => void
+  isSubmit: boolean
+  setIsSubmit: (c: boolean) => void
+  title: string
+  setTitle: (c: string) => void
+  description: string
+  setDescription: (c: string) => void
+  inputFields: any
+  setInputFields: (c: any) => void
+  inputFieldsStep: any
+  setInputFieldsStep: (c: any) => void
+  arrayRecipe: any
+  setArrayRecipe: (c: any) => void
 }
 
 export const AppContext = createContext<GlobalContent>({
   newRecipe: false, // set a default value
   setNewRecipe: () => {},
-  })
-  export const useGlobalContext = () => useContext(AppContext)
+  isSubmit: false,
+  setIsSubmit: () => {},
+  title: '',
+  setTitle: () => {},
+  description: '',
+  setDescription: () => {},
+  inputFields: [{ name: '' }],
+  setInputFields: () => {},
+  inputFieldsStep: [{ name: '' }],
+  setInputFieldsStep: () => {},
+  arrayRecipe:[],
+  setArrayRecipe: (c: any) => {},
+})
+export const useGlobalContext = () => useContext(AppContext)
 
-  function App() {
+interface ABC {
+  title: string;
+  desc: string;
+  name: string;
+  inFi:[]
+  inFS:[]
+}
+
+function App() {
   const [newRecipe, setNewRecipe] = useState(false)
+  const [isSubmit, setIsSubmit] = useState(false)
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [inputFields, setInputFields] = useState([{ name: '' }])
+  const [inputFieldsStep, setInputFieldsStep] = useState([{ name: '' }])
+  const [arrayRecipe, setArrayRecipe] = useState([])
+
   return (
-    <AppContext.Provider value={{newRecipe,setNewRecipe}}>
-    <div className='App'>
-      <Header 
-      addRecipe={()=>setNewRecipe(true)}
-      />
-      {newRecipe ?
-      <Form
-      closeForm={(e)=>{
-        const target = e.target as HTMLTextAreaElement
-       if(target.className==='container-form')
-        {setNewRecipe(false)}
+    <AppContext.Provider
+      value={{
+        newRecipe,
+        setNewRecipe,
+        isSubmit,
+        setIsSubmit,
+        title,
+        setTitle,
+        description,
+        setDescription,
+        inputFields,
+        setInputFields,
+        inputFieldsStep,
+        setInputFieldsStep,
+        arrayRecipe,
+        setArrayRecipe,
       }}
-      />
-      :null}
-      
-    </div>
+    >
+      <div className='App'>
+        <Header addRecipe={() => setNewRecipe(true)} />
+        {newRecipe ? (
+          <Form
+            closeForm={(e) => {
+              const target = e.target as HTMLTextAreaElement
+              if (target.className === 'container-form') {
+                setNewRecipe(false)
+              }
+            }}
+          />
+        ) : null}
+        {isSubmit
+          ?
+          
+          
+          arrayRecipe &&
+            
+             arrayRecipe.flat(Infinity).map((el:ABC,index,arr)=>{
+              console.log(el);
+              
+             return(
+              <div className={'result'} key={index} >
+             <p>{el.title}</p>
+             <p>{el.desc}</p>
+             {el.inFi.map((e:ABC,i:number)=><p key={i}>{e.name}</p>)}
+             {el.inFS.map((e:ABC,i:number)=><p key={i}>{e.name}</p>)}
+              
+             
+             </div>
+             )
+            })
+             
+             
+            
+          : null}
+      </div>
     </AppContext.Provider>
   )
 }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,20 +5,14 @@ import Button from '../Button/Button'
 import './Form.css'
 import { AppContext } from '../../App'
 
-
 type Props = {
   closeForm: (e: React.MouseEvent<HTMLDivElement>) => void
 }
 
-
-
 const Form = ({ closeForm }: Props) => {
-  const { newRecipe,setNewRecipe } = useContext(AppContext)
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [inputFields, setInputFields] = useState([{ name: '' }])
-  const [inputFieldsStep, setInputFieldsStep] = useState([{ name: '' }])
-  const [isSubmit, setIsSubmit] = useState(false)
+  const {title, setTitle,description, setDescription, newRecipe, setNewRecipe, isSubmit,setIsSubmit,inputFields,setInputFields,inputFieldsStep, setInputFieldsStep,arrayRecipe, setArrayRecipe } = useContext(AppContext)
+  
+ 
 
   const handleFormChange = (
     index: number,
@@ -48,98 +42,92 @@ const Form = ({ closeForm }: Props) => {
 
   const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    // setNewRecipe(false)
+    setNewRecipe(false)
     setIsSubmit(true)
+  }
+  const addRecipe = ()=>{
+   let recipe ={title:title,desc:description,inFi:inputFields,inFS:inputFieldsStep}
+   setArrayRecipe([...arrayRecipe,recipe])
+    
   }
 
   return (
-    <>
-    <div
-      className={'container-form'}
-      onClick={closeForm}
-    >
-      <form
-        className={'form'}
-        onSubmit={onSubmitHandler}
+    <div>
+      <div
+        className={'container-form'}
+        onClick={closeForm}
       >
-        <h2 className={'h2'}>Add a new recipe</h2>
-        <Label title={'Title'} />
-        <Input
-          name={'title'}
-          type={'text'}
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <Label title={'Description'} />
-        <Input
-          name={'description'}
-          type={'text'}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <Label title={'Ingredients'} />
-        {inputFields.map((input, index) => {
-          return (
-            <Input
-              key={index}
-              type={'text'}
-              name='name'
-              value={input.name}
-              onChange={(event) => handleFormChange(index, event)}
-            />
-          )
-        })}
-        <Button
-          type='button'
-          label={'ADD INGREDIENT'}
-          bg='#5cc0eb'
-          color='black'
-          onClick={addFields}
-        />
-        
-        <Label title={'Steps'} />
-        {inputFieldsStep.map((input, index) => {
-          return (
-            <Input
-              key={index}
-              type={'text'}
-              name='name'
-              value={input.name}
-              onChange={(event) => handleFormStep(index, event)}
-            />
-          )
-        })}
-        <Button
-          type='button'
-          label={'ADD STEP'}
-          bg='#5cc0eb'
-          color='black'
-          onClick={addFieldsStep}
-        />
+        <form
+          className={'form'}
+          onSubmit={onSubmitHandler}
+        >
+          <h2 className={'h2'}>Add a new recipe</h2>
+          <Label title={'Title'} />
+          <Input
+            name={'title'}
+            type={'text'}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Label title={'Description'} />
+          <Input
+            name={'description'}
+            type={'text'}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <Label title={'Ingredients'} />
+          {inputFields.map((input: { name: string }, index:number) => {
+            return (
+              <Input
+                key={index}
+                type={'text'}
+                name='name'
+                value={input.name}
+                onChange={(event) => handleFormChange(index, event)}
+              />
+            )
+          })}
+          <Button
+            type='button'
+            label={'ADD INGREDIENT'}
+            bg='#5cc0eb'
+            color='black'
+            onClick={addFields}
+          />
 
-        <Button
-          type='submit'
-          label={'SUBMIT'}
-          bg='#5cc0eb'
-          color='black'
-          onClick={(e) => e}
-        />
-      </form>
+          <Label title={'Steps'} />
+          {inputFieldsStep.map((input: { name: string }, index:number) => {
+            return (
+              <Input
+                key={index}
+                type={'text'}
+                name='name'
+                value={input.name}
+                onChange={(event) => handleFormStep(index, event)}
+              />
+            )
+          })}
+          <Button
+            type='button'
+            label={'ADD STEP'}
+            bg='#5cc0eb'
+            color='black'
+            onClick={addFieldsStep}
+          />
+
+          <Button
+            type='submit'
+            label={'SUBMIT'}
+            bg='#5cc0eb'
+            color='black'
+            onClick={() =>addRecipe()}
+          />
+        </form>
+      </div>
+
       
     </div>
-    {isSubmit ? (
-        <div
-          className='result'
-          style={{ color: 'white' }}
-        >
-          <h3>title: {title}</h3>
-          <p>description:{description}</p>
-          {inputFields.map((input, index) => {
-            return <p key={index}>{input.name}</p>
-          })}
-        </div>
-      ) : null}
-    </>
   )
 }
 
